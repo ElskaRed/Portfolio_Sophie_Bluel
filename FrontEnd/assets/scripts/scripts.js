45e7292b2e6c00acdeeb84edc7f0bbc3d6cf7901
Roxanne Vignon <vignon.roxanne@gmail.com>
2023-07-14 10:47:56 +0200

    Remaniement fonctions fetch et affichage

--- FrontEnd/assets/scripts/scripts.js
+++ FrontEnd/assets/scripts/scripts.js
@@ -1,36 +1,57 @@
-// Fonction permettant la récupération dynamique des travaux depuis l'API
+// Fonction pour afficher les travaux
 
-fetch("http://localhost:5678/api/works",{
-  headers: {"accept": "application/json", "Content-Type": "application/json"}
-})
-  .then(response => response.json())
+const AfficherTravaux = (data) => {
+  data.forEach((item) => {
+    const imageUrl = item.imageUrl;
+    const description = item.title;
+
+    const gallery = document.querySelector(".gallery");
+    const figureElement = document.createElement("figure");
+    gallery.appendChild(figureElement);
+
+    const imgElement = document.createElement("img");
+    imgElement.src = imageUrl;
+    imgElement.alt = item.title;
+    figureElement.appendChild(imgElement);
+
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.textContent = item.title;
+    figureElement.appendChild(figcaptionElement);
+  });
+};
+
+// fonction pour récupérer les travaux depuis l'API
+
+const getWorks = () => {
+  return fetch("http://localhost:5678/api/works",{
+    headers: {"accept": "application/json", "Content-Type": "application/json"}
+  })
+    .then(response => response.json())
+    .catch(error => {
+      console.log("Une erreur s\'est produite:", error);
+    });
+};
+
+// Appel de la fonction de récupération + affichage des travaux
+
+getWorks()
   .then(data => {
     console.log(data);
-    data.forEach((item, index) => {
-      const imageUrl = item.imageUrl;
-      const description = item.title;
-
-      const gallery = document.querySelector(".gallery");
-      const figureElement = document.createElement("figure");
-      gallery.appendChild(figureElement);
-
-      const imgElement = document.createElement("img");
-      imgElement.src = imageUrl;
-      imgElement.alt = item.title;
-      figureElement.appendChild(imgElement);
-
-      const figcaptionElement = document.createElement("figcaption");
-      figcaptionElement.textContent = item.title;
-      figureElement.appendChild(figcaptionElement);
-    });
-  })
-  .catch(error => {
-    console.log("Une erreur s\'est produite:", error);
+    AfficherTravaux(data);
   });
 
 // Filtres
-    //Visuel, changement de classe CSS
+
+fetch ("http://localhost:5678/api/category",{
+  headers: {"accept": "application/json", "Content-Type": "application/json"}
+})
+
 const filtresBoutons = document.querySelectorAll('.filtres__boutons');
+const gallery = document.querySelector("gallery");
+
+filtresBoutons.forEach(bouton => {
+  bouton.addEventListener("click", selectionBouton);
+});
 
 const selectionBouton = (event) => {
   filtresBoutons.forEach(bouton => {
@@ -39,7 +60,5 @@ const selectionBouton = (event) => {
   event.target.classList.add("filtres__boutons--select");
 };
 
-filtresBoutons.forEach(bouton => {
-  bouton.addEventListener("click", selectionBouton);
-});
+
 
