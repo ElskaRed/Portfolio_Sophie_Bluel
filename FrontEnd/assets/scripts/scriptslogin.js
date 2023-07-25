34a5184efe4ddfef9a07938f2275ea3da04ca4a1
Roxanne Vignon <vignon.roxanne@gmail.com>
2023-07-25 11:12:02 +0200

    login en cours

--- FrontEnd/assets/scripts/scriptslogin.js
+++ FrontEnd/assets/scripts/scriptslogin.js
@@ -1,31 +1,51 @@
-document.getElementById("login__form").addEventListener("submit", function(event) {
-  event.preventDefault();
+async function loginUser() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-      alert("Le format de l'e-mail est incorrect. Veuillez entrer une adresse e-mail valide.");
-      return;
-  }
-  const formData = {
-      email: email,
-      password: password
-  };
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const emailPattern = /^[a-zA-Z\d.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+        console.log(email, password);
 
-  fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(formData)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error("Une erreur s'est produite :", error);
-});
-});
+        if (!email.match(emailPattern)) {
+            alert("Le format de l'e-mail est incorrect. Veuillez entrer une adresse e-mail valide.");
+            return;
+        }
+        
+        if (password.trim() === "") {
+            alert("Veuillez entrer un mot de passe.");
+            return;
+        }
+        
+        const user = {
+            email: email,
+            password: password
+        };
+        
+        //try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+            
+            const data = await response.json();
+            console.log(data);
+            
+            if (data.userId === 1) {
+                window.location.href = "index.html?isAdmin=true";
+            } else {
+                alert("Utilisateur inconnu")
+            };
+
+      /* } catch (error) {
+            console.error("Une erreur s'est produite :", error);
+        } */
+    });
+}
+
+loginUser();
